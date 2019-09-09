9c66f7d0ffea1a59b284a05d9d0891cd50d2a085
Carmelo Campos <31357533+CarmeloCampos@users.noreply.github.com>
2019-09-09 17:35:44 -0500

    Fix #4
    Fix #4

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -45,7 +45,7 @@ double GetDifficulty(const CBlockIndex* blockindex)
 
 double GetPoWMHashPS()
 {
-    if (pindexBest->nHeight >= LAST_POW_BLOCK)
+    if (pindexBest->nHeight >= LAST_POW_BLOCK && pindexBest->nHeight < POW_RE_ENABLE)
         return 0;
 
     int nPoWInterval = 72;
